1e37fa70ac8b74d841ed89c5c2f15b1e09f6f48f
Andrea Sant <andrea.sant@bookingandco.com>
2025-10-27 14:49:54 +0100

    Applied migration AddedTradeIndexByUserSecurity

---
+++ src/Trading.Infrastructure.Data/Migrations/Postgres/20251027134700_AddedTradeIndexByUserSecurity.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Trading.Infrastructure.Data.Migrations.Postgres
+{
+    /// <inheritdoc />
+    public partial class AddedTradeIndexByUserSecurity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Trades_UserId_SecurityId",
+                table: "Trades",
+                columns: new[] { "UserId", "SecurityId" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Trades_UserId_SecurityId",
+                table: "Trades");
+        }
+    }
+}
