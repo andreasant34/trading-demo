2a1cd9dfa16a2e6c12f382b90914a4fee2af86c4
Andrea Sant <andrea.sant@bookingandco.com>
2025-10-28 12:14:53 +0100

    UserRepositoryTests

---
+++ tests/Trading.Infrastructure.Data.Tests/SeededTradingDbContext.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Trading.Core.Entities;
+
+namespace Trading.Infrastructure.Data.Tests
+{
+    internal class SeededTradingDbContext : TradingDbContext
+    {
+        public ISet<int> SeededUserIDs { get; private set; }
+        public ISet<string> SeededEmails { get; private set; }
+
+        public SeededTradingDbContext(DbContextOptions<TradingDbContext> dbContextOptions) 
+            : base(dbContextOptions)
+        {
+            SeededUserIDs = new HashSet<int>();
+            SeededEmails = new HashSet<string>();
+        }
+
+        public void SeedAllData()
+        {
+            SeedUserData();
+        }
+
+        public void SeedUserData()
+        {
+            int investmentAccountIdCounter = 1;
+            for (var userIdCounter = 1; userIdCounter <= 5; userIdCounter++)
+            {
+                var newUser = new UserEntity
+                {
+                    Id = userIdCounter,
+                    Name = $"Name {userIdCounter}",
+                    Surname = $"Surname {userIdCounter}",
+                    Email = $"name{userIdCounter}@gmail.com",
+                    InvestmentAccounts = new List<InvestmentAccountEntity>()
+                };
+
+                SeededUserIDs.Add(newUser.Id);
+                SeededEmails.Add(newUser.Email);
+                Users.Add(newUser);
+
+                for (var investmentAccountCounter = 1; investmentAccountCounter <= 2; investmentAccountCounter++)
+                {
+                    var investmentAccount = new InvestmentAccountEntity
+                    {
+                        Id = investmentAccountIdCounter,
+                        Name = $"User {userIdCounter} Investment Account {investmentAccountCounter}",
+                        UserId = userIdCounter
+                    };
+
+                    InvestmentAccounts.Add(investmentAccount);
+                    investmentAccountIdCounter++;
+                }
+
+            }
+
+            SaveChanges();
+        }
+    }
+}
---
+++ tests/Trading.Infrastructure.Data.Tests/UserRepositoryTests.cs
@@ -0,0 +1,49 @@
+using Microsoft.EntityFrameworkCore;
+using Trading.Infrastructure.Data.Repositories;
+
+namespace Trading.Infrastructure.Data.Tests;
+
+public class UserRepositoryTests
+{
+    [Fact]
+    public async Task ListUsers_ShouldReturnAllUsers()
+    {
+        var context = InitSeededTradingDbContext();
+        var userRepository = new UserRepository(context);
+
+        var allUsers = await userRepository.ListUsersAsync();
+        Assert.Equal(allUsers.Count(), context.SeededUserIDs.Count());
+    }
+
+    [Fact]
+    public async Task GetUserById_ShouldReturnUser_When_User_Exists()
+    {
+        var context = InitSeededTradingDbContext();
+        var userRepository = new UserRepository(context);
+
+        var existingUser = await userRepository.GetUserByIdAsync(context.SeededUserIDs.First());
+        Assert.NotNull(existingUser);
+    }
+
+    [Fact]
+    public async Task GetUserById_ShouldReturnNull_When_User_NotExists()
+    {
+        var context = InitSeededTradingDbContext();
+        var userRepository = new UserRepository(context);
+
+        var notFoundUser = await userRepository.GetUserByIdAsync(context.SeededUserIDs.Max() + 1);
+        Assert.Null(notFoundUser);
+    }
+
+    private static SeededTradingDbContext InitSeededTradingDbContext()
+    {
+        var options = new DbContextOptionsBuilder<TradingDbContext>()
+                            .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                            .Options;
+
+        var context = new SeededTradingDbContext(options);
+        context.Database.EnsureCreated();
+        context.SeedUserData();
+        return context;
+    }
+}
